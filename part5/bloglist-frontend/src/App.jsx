1bfc941c7200480fdbb871fc9f9cf189d18da546
Danh-Lan <danhlan.nguyen1@gmail.com>
2025-04-23 23:04:05 +0200

    part 5, 5.1 to 5.7

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,27 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
+import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
+import Togglable from './components/Togglable'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
   const [blogs, setBlogs] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [blogFormVisible, setBlogFormVisible] = useState(false)
+
+  const hideWhenVisible = { display: blogFormVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogFormVisible ? '' : 'none' }
+
+  const [notification, setNotification] = useState({ message: null})
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +29,101 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const notifyWith = (message, isError = false) => {
+    setNotification({ message, isError })
+    setTimeout(() => {
+      setNotification({ message: null })
+    }, 5000)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      notifyWith(`wrong username or password`, true)
+    }
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    setUsername('')
+    setPassword('')
+    window.localStorage.removeItem('loggedBlogappUser')
+  }
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      const createdBlog = await blogService.create({
+        title, author, url
+      })
+
+      setBlogs([...blogs, createdBlog])
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      
+      notifyWith(`a new blog ${createdBlog.title} by ${createdBlog.author} added`)
+    } catch (exception) {
+      notifyWith(`error with the server, please try again`, true)
+      console.log('error :', exception.message)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Notification notification={notification} />
+        <LoginForm
+          username={username}
+          password={password}
+          handleUsernameChange={({ target }) => setUsername(target.value)}
+          handlePasswordChange={({ target }) => setPassword(target.value)}
+          handleSubmit={handleLogin}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <Notification notification={notification} />
+      <p>{user.name} logged in<button onClick={handleLogout}>logout</button></p>
+      <Togglable buttonLabel='new blog'>
+        <BlogForm 
+          title={title}
+          author={author}
+          url={url}
+          handleTitleChange={({ target }) => setTitle(target.value)}
+          handleAuthorChange={({ target }) => setAuthor(target.value)}
+          handleUrlChange={({ target }) => setUrl(target.value)}
+          handleSubmit={handleCreateBlog}
+        />
+      </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  return (
+    blogVisible ? (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={() => setBlogVisible(false)}>hide</button><br/>
+        {blog.url}<br/>
+        likes {blog.likes} <button>like</button><br/>
+        {blog.user.name}<br/>
+      </div>
+    ) : (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={() => setBlogVisible(true)}>show</button>
+      </div>
+    )
+  )
+}
 
 export default Blog
